dda6a7e4067c2c049776bab94edfde4e72c076d0
zvef <jakekneece7@gmail.com>
2023-05-16 12:29:21 -0400

    Finished exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let char = word.split('');
+    let size = char.length;
+    let output = [];
+    let result;
+    for (let i = 0; i < size; i ++) {
+        output.unshift(char[i]);
+    }
+    result = output.toString();
+    result = result.replaceAll(',', '');
+    return result;
 };
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
